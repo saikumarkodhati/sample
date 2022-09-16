171dc420e986c92828eeef9cf60f15579a2295d9
saikumar kodhati <kodhatisaikumar521@gmail.com>
2022-09-16 09:53:16 +0530

    sample

---
+++ DigitalBook/DigitalBook/Controllers/BooksController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DigitalBook.Models;
+
+namespace DigitalBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        BooksDBContext db = new BooksDBContext();
+        public IEnumerable<SearchBook> Get()
+        {
+            return db.SearchBooks;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] SearchBook searchbook)
+        {
+            db.SearchBooks.Add(searchbook);
+            db.SaveChanges();
+            var response = new { Status = "Success" };
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public IActionResult Put(SearchBook searchbook)
+        {
+            db.SearchBooks.Add(searchbook);
+            db.Entry(searchbook).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            var response = new { Status = "Success" };
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var data = db.SearchBooks.Where(x => x.Id == id).FirstOrDefault();
+            db.SearchBooks.Remove(data);
+            db.SaveChanges();
+            var response = new { Status = "Success" };
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("Search")]
+        public IActionResult Post(string author)
+        {
+            if (author == null)
+            {
+                return BadRequest("Invalid search options");
+            }
+
+            var searchResult = db.SearchBooks.Where(x => x.Author == author).FirstOrDefault();
+
+            return Ok(searchResult);
+        }
+
+        // Create Book API
+    }
+}
---
+++ DigitalBook/DigitalBook/Controllers/CreateBookController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DigitalBook.Models;
+
+namespace DigitalBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateBookController : ControllerBase
+    {
+        BooksDBContext db = new BooksDBContext();
+        public IEnumerable<CreateBook> Get()
+        {
+            return db.CreateBooks;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateBook createbook)
+        {
+            db.CreateBooks.Add(createbook);
+            db.SaveChanges();
+            var response = new { Status = "Success" };
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public IActionResult Put(CreateBook createbook)
+        {
+            db.CreateBooks.Add(createbook);
+            db.Entry(createbook).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            var response = new { Status = "Success" };
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var data = db.CreateBooks.Where(x=>x.Id == id).FirstOrDefault();
+            db.CreateBooks.Remove(data);
+            db.SaveChanges();
+            var response = new { Status = "Success" };
+            return Ok(response);
+        }
+
+        //[HttpPost]
+        //[Route("Search")]
+        //public IActionResult Post(string author)
+        //{
+        //    if (author == null)
+        //    {
+        //        return BadRequest("Invalid search options");
+        //    }
+
+        //    var searchResult = db.SearchBooks.Where(x => x.Author == author).FirstOrDefault();
+
+        //    return Ok(searchResult);
+        //}
+    }
+}
